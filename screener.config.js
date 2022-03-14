2325e6a6a46f533a5ac3b4f09a666dd045975bd8
Anthony Brown <anthony.brown@saucelabs.com>
2022-03-14 15:15:23 -0500

    init commit

---
+++ screener.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    projectRepo: 'abrown/storyBook',
+    storybookConfigDir: '.storybook',
+    // storybookStaticDir: '<directory>',
+    apiKey: process.env.SCREENER_API_KEY,
+    resolution: '1920x1080'
+  };
